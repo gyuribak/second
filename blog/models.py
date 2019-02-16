131691bbbb7e0d36140d32dbedfa4099a8ab52be
gyuribak <37536407+gyuribak@users.noreply.github.com>
2019-02-16 14:10:57 +0900

    first blog

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date pulished')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Blog
+
+def home(request):
+    blogs = Blog.objects #쿼리셋
+    return render(request, 'home.html', {'blogs':blogs})
